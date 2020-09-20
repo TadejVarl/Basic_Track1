5c4e976a94fccb3427f7856499b5ce3063cfa4e7
tadejvarl <Varl.to.5650>
2020-09-20 22:15:42 +0200

    parenthesis changes the results

---
+++ week_2/Exercise 2.14.3.py
@@ -0,0 +1,11 @@
+
+result1 = 6 * 1 - 2
+#this is a test comment  that will ruin the result result2 = 6 * (1 - 2)
+
+print(result1)
+print(result2)
+
+#Traceback (most recent call last):
+# File "/Users/tadejvarl/PycharmProjects/Basic_Track1/week_2/Exercise 2.14.3.py", line 6, in <module>
+# print(result2)
+# NameError: name 'result2' is not defined
