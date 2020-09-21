6ed59fc8d936f67957e779f14116eec047a9ab32
tadejvarl <Varl.to.5650>
2020-09-21 15:33:58 +0200

    New version of how long to wait till the alarm goes off?

--- week_2/Exercise 2.14.7.py
+++ week_2/Exercise 2.14.7.py
@@ -1,6 +1,7 @@
-current_time = 2
+time_now_1 = input ('What is the time now?')
+time_now = int (time_now_1)
 
-time_until_alarm = 51
+hours_to_wait_1 = input ('How long to wait for the alarm? :')
+hours_to_wait = int(hours_to_wait_1)
 
-final_answer = (current_time + time_until_alarm ) % 24
-
+print ((hours_to_wait % 24) + time_now)
