cc90164f7035408108399f24cce54b6e6bc7d74b
tadejvarl <Varl.to.5650>
2020-09-24 17:24:15 +0200

    Modified version, with sorted out weaked alerts

--- week_2/Exercise 2.14.5.py
+++ week_2/Exercise 2.14.5.py
@@ -1,11 +1,12 @@
-#first define the variables
+# first define the variables
 
 
-r = 1/100 #realistic nominal interest rate
-n = 3 #yearly compounding frequency
-t = 10 #number of years
-P = int(input("You're opening a savings account for the duration of 10 years. What is your principal amount in dollars: "))
+r = 1 / 100  # realistic nominal interest rate
+n = 3  # yearly compounding frequency
+t = 10  # number of years
+P = int(
+    input("You're opening a savings account for the duration of 10 years. What is your principal amount in dollars: "))
 
-A = P * (1 + r/n)**(n*t)
+A = P * (1 + r / n) ** (n * t)
 
-print("Your savings at the end of year ", t , " will amount for: ", A)
+print("Your savings at the end of year ", t, " will amount for: ", A)
--- week_2/Exercise 2.14.7.py
+++ week_2/Exercise 2.14.7.py
@@ -1,7 +1,7 @@
-time_now_1 = input ('What is the time now?')
-time_now = int (time_now_1)
+time_now_1 = input('What is the time now?')
+time_now = int(time_now_1)
 
-hours_to_wait_1 = input ('How long to wait for the alarm? :')
+hours_to_wait_1 = input('How long to wait for the alarm? :')
 hours_to_wait = int(hours_to_wait_1)
 
-print ((hours_to_wait % 24) + time_now)
+print((hours_to_wait % 24) + time_now)
